48eb88b78817e26d5c4bbf6b2c8d59f9fd362cce
Simoghuj <skladsim@seznam.cz>
2020-01-15 22:04:07 +0100

    cleared

--- Line_Folower01/Line_Folower01/main.cpp
+++ Line_Folower01/Line_Folower01/main.cpp
@@ -7,49 +7,45 @@
 
 #include "3piLibPack.h"
 
-#define Motor_ol 0,50
-#define Motor_or 50,0
-#define Motor_rov 50,50
+#define Motor_ol 0,100
+#define Motor_or 100,0
+#define Motor_rov 100,100
 
 #define tim 100
 #define rmot 60
 #define lmot 20
 
-bool white_line = true;
-
-int senzor0 = 0;
-int senzor1 = 0;
-int senzor2 = 0;
-int senzor3 = 0;
-int senzor4 = 0;
-int bila = 0;
-int position = 0;
-
 void run(void)
 {
-	
-	waitForButton(BUTTON_A);
-	cal_round();
-	display.clear();
-	display.printNumToXY(getBatteryVoltage(), 2,0);
-	display.printToXY("mV", 6,0);
-	delay(1000);
-	display.clear();
+	buzzer.start();
+	delay(100);
+	buzzer.stop();
 	display.printToXY("press", 1, 0);
 	display.printToXY("Button_B", 0, 1);
+	cal_round();
+	delay (1000);
+	display.clear
+	display.printNumToXY(getBatteryVoltage(), 2,0);
+	display.printToXY("mV", 6,0);
+	
+	bool white_line = true;
+	
+	int senzor0 = 0;
+	int senzor1 = 0;
+	int senzor2 = 0;
+	int senzor3 = 0;
+	int senzor4 = 0;
+	int bila = 0;
 	
 	waitForButton(BUTTON_B);
 	display.clear();
-	buzzer.start();
-	display.print("Start!!");
-	delay(100);
-	buzzer.stop();
-	display.clear();
+	
+	
 	
 	
 	while (1) 
     {
-
+		
 		while(1)
 		{
 			
@@ -61,15 +57,17 @@ void run(void)
 			
 			bila = senzor0 + senzor1 + senzor2 + senzor3 + senzor4;
 			
-			position = getLinePos(white_line = false);
-		
-			if(bila < 15) // vyjel si z èáry
-			{
+			int position = getLinePos(white_line = false);
+			rs232.sendNumber(bila);
+			rs232.send("\n");
 			
-				setMotorPower(125,125);
+			if(bila < 100) // vyjel si z èáry
+			{
+				setMotorPower(50,50);
 				delay (400);
-				for(int i;i!=4 ;i++){
-					setMotorPower(-125,-125);
+				for(int i;i!=4 ;i++)
+				{
+					setMotorPower(-50,-50);
 					delay(100);
 					setMotorPower(rmot,lmot);
 					delay(tim);
@@ -80,26 +78,27 @@ void run(void)
 					setMotorPower(-lmot,-rmot);
 					delay(tim);
 					tim+100;
+				
+				}	
 			}
-			
-			if(position < 1024)//zatoè vlevo (nefunguje s0 proto 2000 jinak 1000)
+			else if(position < 1024)//zatoè vlevo (nefunguje s0 proto 2000 jinak 1000)
 			{
-				setMotorPower(Motor_ol);	
+				setMotorPower(Motor_ol);
+				
+				
+				
 			}
-			
-			else if(position < 3048) //èára pod tebou
+			else if(position < 3072) //èára pod tebou
 			{
 				setMotorPower(Motor_rov);	
 			}
 			
-			else  // zatoè vpravo pokud je position < 4000
+			else if(position <= 4096) // zatoè vpravo pokud je position < 4000
 			{
 				setMotorPower(Motor_or);
 				
 			}	
 			
-			
-			}
 		}
     }
 }
